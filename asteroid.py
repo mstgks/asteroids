f5ab1fbe3cdef6f991c6aa1e5b4a527f74e7c479
mstgks <michael.stagkos@outlook.com>
2026-01-27 05:20:28 +0200

    Asteroids boot.dev

---
+++ asteroid.py
@@ -0,0 +1,28 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+from logger import log_event
+import random
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            old_radius = self.radius
+            angle = random.uniform(20, 50)
+            velocity_one = self.velocity.rotate(angle)
+            velocity_two = self.velocity.rotate(angle)
+            new_radius = old_radius - ASTEROID_MIN_RADIUS
+            asteroid_one = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_two = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_one.velocity = velocity_one * 1.2
+            asteroid_two.velocity = velocity_two * 1.2
